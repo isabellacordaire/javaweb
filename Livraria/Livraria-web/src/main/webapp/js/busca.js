c3980ae8ea60194a7dc5cdcf8b9187eeee552f68
javasabmat <javasabmat@GYNLAB-JED01>
2017-06-03 12:50:50 -0300

    Laboratorio 9

--- Livraria/Livraria-web/src/main/webapp/js/busca.js
+++ Livraria/Livraria-web/src/main/webapp/js/busca.js
@@ -1,42 +1,35 @@
 function buscarProduto(titulo){
 var produtos=[
-{"livros" : ["BRANCA DE NEVE", "A BELA MOÇA", "O GUIA", "DR.
-OZZY", "PEQUENO IRMÃO"],
-PIE", "HANNIBAL"],
-"filmes" : ["DEADPOOL", "GODZILLA", "LAGOA AZUL", "AMERICAN
-"musicas": ["ZZ TOP", "BLACK SABBATH", "AC DC", "PRIMUS",
-"MOLEJO"],
-"games": ["MIRRORS EDGE", "FIFA", "BATTLEFIELD", "UNDERTALE",
-"SUPERHOT"]}
+{"livros" : ["BRANCA DE NEVE", "A BELA MOÇA", "O GUIA", "DR.OZZY", "PEQUENO IRMÃO"],
+"filmes" : ["DEADPOOL", "GODZILLA", "LAGOA AZUL", "AMERICAN PIE", "HANNIBAL"],
+"musicas": ["ZZ TOP", "BLACK SABBATH", "AC DC", "PRIMUS", "MOLEJO"],
+"games": ["MIRRORS EDGE", "FIFA", "BATTLEFIELD", "UNDERTALE", "SUPERHOT"]}
 ];
 var nome = titulo.value.toUpperCase();
 var enc = 0;
 for(var i=0; i < produtos.length ; i++){
-de jogos");
+
 for(var v=0; v < produtos[i].games.length; v++){
 if(produtos[i].games[v] == nome){
-window.alert("Temos "+ titulo.value +" em nosso estoque
+window.alert("Temos "+ titulo.value +" em nosso estoque de jogos");
 }
 enc++;
 }
 for(var v=0; v < produtos[i].livros.length; v++){
 if(produtos[i].livros[v] == nome){
-window.alert("Temos "+ titulo.value +" em nosso estoque
-de livros");
+window.alert("Temos "+ titulo.value +" em nosso estoque de livros");
 enc++;
 }
 }
 for(var v=0; v < produtos[i].filmes.length; v++){
 	if(produtos[i].filmes[v] == nome){
-	window.alert("Temos "+ titulo.value +" em nosso estoque
-	de filmes");
+	window.alert("Temos "+ titulo.value +" em nosso estoque	de filmes");
 	enc++;
 	}
 	}
 for(var v=0; v < produtos[i].musicas.length; v++){
 	if(produtos[i].musicas[v] == nome){
-	window.alert("Temos "+ titulo.value +" em nosso estoque
-	de músicas");
+	window.alert("Temos "+ titulo.value +" em nosso estoque de músicas");
 	enc++;
 	}
 	}
@@ -45,20 +38,16 @@ if(titulo.value == ""){
 window.alert("Busca vazia!");
 }
 else if(nome == "LIVROS" || nome == "LIVRO"){
-window.alert("Encontramos alguns produtos nessa categoria LIVROS:
-"+produtos[0].livros);
+window.alert("Encontramos alguns produtos nessa categoria LIVROS:"+produtos[0].livros);
 }
 else if(nome == "FILMES" || nome == "FILME"){
-window.alert("Encontramos alguns produtos na categoria FILMES:
-"+produtos[0].filmes);
+window.alert("Encontramos alguns produtos na categoria FILMES: "+produtos[0].filmes);
 }
 else if(nome == "MUSICAS" || nome == "MUSICA"){
-window.alert("Encontramos alguns produtos nessa categoria MUSICAS:
-"+produtos[0].musicas);
+window.alert("Encontramos alguns produtos nessa categoria MUSICAS: "+produtos[0].musicas);
 }
 else if(nome == "GAMES" || nome == "JOGOS"){
-window.alert("Encontramos alguns produtos nessa categoria GAMES:
-"+produtos[0].games);
+window.alert("Encontramos alguns produtos nessa categoria GAMES: "+produtos[0].games);
 }
 else if(enc == 0){
 window.alert("Produto não encontrado!");
